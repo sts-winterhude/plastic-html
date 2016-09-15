114792dd5396f1397753cb8aa66c4e5d0a7bbc7b
Tom Scholz <ts@scholzworks.com>
2016-09-15 14:23:47 +0200

    nerv

--- js/init.js
+++ js/init.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $('ul.tabs').tabs();
-    $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav({
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
     $('.parallax').parallax();
     $('.tooltipped').tooltip({delay: 50});
     $('.modal-trigger').leanModal();
