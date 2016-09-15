4494dc2cc742a5d90f80e423ceacc6abc549fd6d
Tom Scholz <ts@scholzworks.com>
2016-09-15 14:17:20 +0200

    Toggle and other stuff

--- js/init.js
+++ js/init.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
     $('.tooltipped').tooltip({delay: 50});
+    $('.modal-trigger').leanModal();
 });
+
+$('.trotzdem').click( function() {
+    $("#table").toggleClass("hide-on-small-and-down");
+} );
